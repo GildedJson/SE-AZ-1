8a41bce6fb1e1d2e70cba682dc1f375c8836d4bc
abdossamad haghiri <abdossamadhaghiri@gmail.com>
2023-07-15 10:58:07 +0330

    check the input

--- Code.py
+++ Code.py
@@ -1,9 +1,11 @@
 def main():
     q = input('Ask a math question')
     splitq = q.split(' ')
-    if splitq[1] == '+':
+    if len(splitq) != 3:
+	print ("incorect number")
+    elif splitq[1] == '+':
         print (int(splitq[0]) + int(splitq[2]))
-    if splitq[1] == '-':
+    elif splitq[1] == '-':
         print (int(splitq[0]) - int(splitq[2]))
 
 
